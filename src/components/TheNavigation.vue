b49f8922528cc1b5f3bce1648564bfc23cd85c2d
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2022-10-06 16:33:26 +0300

    app started :zap:

---
+++ src/components/TheNavigation.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="nav">
+    <router-link to ="/">Home</router-link>
+    <router-link to ="/about">About</router-link>
+    <router-link to = "/brazil">Brazil</router-link>
+    <router-link to = "/hawaii">Hawaii</router-link>
+    <router-link to = "/panama">Panama</router-link>
+    <router-link to = "/jamaica">Jamaica</router-link>
+  </div>
+
+</template>
+
+
+<style lang="css">
+
+
+#nav .vue-school-active-link{
+  color: royalblue;
+  border-bottom: 2px solid goldenrod;
+}
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import HomePage from '@/views/HomePage.vue'
+import {createRouter,createWebHistory} from "vue-router";
+
+
+
+
+
+const routes = [
+    {path:"/", name: HomePage, component: HomePage},
+    {path: "/about", name : "AboutPage", component: ()=>import("@/views/AboutPage.vue")},
+    {path: "/brazil", name: "Brazil", component: ()=>import('@/views/Brazil.vue')},
+    {path: "/hawaii", name: "Hawaii", component: ()=>import("@/views/Hawaii.vue")},
+    {path: "/panama", name: "Panama", component: ()=>import("@/views/Panama.vue")},
+    {path: "/jamaica", name: "Jamaica", component: ()=>import("@/views/Jamaica.vue")},
+]
+
+const router = createRouter({
+    history:createWebHistory(),
+    routes,
+    linkActiveClass:"vue-school-active-link"
+})
+
+export default router
